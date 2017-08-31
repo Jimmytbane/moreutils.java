f66314d9aa41ca2c361019092707a4113dc0f4f7
jmfgdev <jmfgdev@outlook.com>
2017-08-31 00:18:40 -0400

    first commit

---
+++ downcasethis.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class downcasethis {
+    public static void main (String[] args) {
+        System.out.println("---DOWNCASETHIS---");
+        System.out.print("Enter the string you would like to downcase  --> ");
+    	Scanner scan = new Scanner(System.in);
+	// String s = scan.next();
+	String input = scan.nextLine();
+	scan.close();
+	input = input.toLowerCase();
+	System.out.printf("Your downcased string  --> %s%n", input);
+    }
+}
